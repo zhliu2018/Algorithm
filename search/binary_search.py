1c4e138871b2d01314b169df6036e25922264028
zhliu2018 <40764228+zhliu2018@users.noreply.github.com>
2019-07-13 20:29:07 +0800

    增加递归实现

--- search/binary_search.py
+++ search/binary_search.py
@@ -14,6 +14,20 @@ def binary_search(nums, target):
             return mid
     return -1
 
+# 递归实现
+def binary_serach(nums, target):
+    if len(nums) == 0:
+        return -1
+    else:
+        mid = len(nums) // 2
+        if nums[mid] == target:
+            return mid
+        else:
+            if target < nums[mid]:
+                return binary_search(nums[:mid], target)
+            else:
+                return binary_search(nums[mid+1:], target)
+
 if __name__ == "__main__":
     test_data = [0, 1, 2, 8, 13, 17, 19, 32, 42]
     print(binary_search(test_data, 8))
